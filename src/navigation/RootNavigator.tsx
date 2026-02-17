57c4ab4abdb66b8cc4b67a49e6367aa3c568bbf5
luisantonioj <luisantoniojopia@gmail.com>
2026-02-17 09:27:20 +0800

    feat(navigation): add RootNavigator with modal support for application form

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MainTabNavigator } from './MainTabNavigator';
+import { ApplicationFormScreen } from '../screens/ApplicationFormScreen';
+import { RootStackParamList } from './types';
+import { useTheme } from '../contexts/ThemeContext';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export const RootNavigator: React.FC = () => {
+  const { colors } = useTheme();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: colors.background },
+        }}
+      >
+        <Stack.Screen name="MainTabs" component={MainTabNavigator} />
+        <Stack.Screen
+          name="ApplicationForm"
+          component={ApplicationFormScreen}
+          options={{
+            presentation: 'modal',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
