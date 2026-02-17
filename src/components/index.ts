a9d0d415f49d39a30d0744ae3f8068e9ffb1c618
luisantonioj <luisantoniojopia@gmail.com>
2026-02-17 09:24:19 +0800

    feat: add index in components folder

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { SearchBar } from './SearchBar';
+export { JobCard } from './JobCard';
+export { ThemeToggle } from './ThemeToggle';
